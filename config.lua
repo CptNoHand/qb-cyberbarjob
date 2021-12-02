33f14aa316404f29e7bb0d63fb268f731892b206
Phantom <59034736+lilphantom25@users.noreply.github.com>
2021-12-02 02:59:15 +0000

    fixed no target use issue

--- config.lua
+++ config.lua
@@ -2,35 +2,35 @@ Config = {}
 
 Config.SQL = 'oxmysql' --- oxmysql or ghmattisql
 
---if u are not using qb-target uncomment this.
+Config.Eye = "true" -- qb-target = true | no target = false
 
---[[Config.Locations = {
-     ["duty"] = {
-         [1] = vector4(324.07, -926.69, 29.25, 99.09),
-     },    
-     ["tray1"] = {
-         [1] = vector4(338.53, -912.01, 29.26, 265.24),
-     },
-     ["tray2"] = {
-         [1] = vector4(333.43, -908.87, 29.26, 89.85),
-     }, 
-     ["pumps"] = {
-         [1] = vector4(338.21, -910.06, 29.26, 271.27),
-         [2] = vector4(333.55, -910.36, 29.26, 98.42),
-     },
-     ["mixer"] = {
-         [1] = vector4(337.47, -914.49, 29.26, 247.69),
-     },    
-     ["fridge"] = {
-         [1] = vector4(335.74, -915.11, 29.26, 181.81),
-     },      
-     ["storage"] = { -- if not using the storage comment this out (uncomment in menu.lua if using)
-         [1] = vector4(0 , 0 , 0, 0),
-     },
-     ["cashregister"] = {
-         [1] = vector4(334.22, -914.15, 29.26, 89.78),
-     },
-  }]]
+Config.Locations = {
+    ["duty"] = {
+        [1] = vector4(324.07, -926.69, 29.25, 99.09),
+    },    
+    ["tray1"] = {
+        [1] = vector4(338.53, -912.01, 29.26, 265.24),
+    },
+    ["tray2"] = {
+        [1] = vector4(333.43, -908.87, 29.26, 89.85),
+    }, 
+    ["pumps"] = {
+        [1] = vector4(338.21, -910.06, 29.26, 271.27),
+        [2] = vector4(333.55, -910.36, 29.26, 98.42),
+    },
+    ["mixer"] = {
+        [1] = vector4(337.47, -914.49, 29.26, 247.69),
+    },    
+    ["fridge"] = {
+        [1] = vector4(335.74, -915.11, 29.26, 181.81),
+    },      
+    ["storage"] = { -- if not using the storage comment this out (uncomment in menus.lua if using)
+        [1] = vector4(0 , 0 , 0, 0),
+    },
+    ["cashregister"] = {
+        [1] = vector4(334.22, -914.15, 29.26, 89.78),
+    },
+}
 
 
 Config.Items = {
